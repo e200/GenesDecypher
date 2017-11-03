884736829a8f664bb9abc8f2649182d345d4b82a
e200 <eleandro@inbox.ru>
2017-11-03 13:14:32 +0100

    Movidos arquivos de biblioteca para a pasta lib

---
+++ lib/definicoes.h
@@ -0,0 +1,15 @@
+/**
+ * Limite máximo de amino ácidos
+ * na nossa proteína.
+ */
+#define MAX_AMIN_ACID_NA_PROT 1000
+
+/**
+ * Limite de codóns.
+ */
+#define MAX_CODON 4
+
+/**
+ * Limite de sequências genéticas.
+ */
+#define MAX_SEQ_GEN 10000
---
+++ lib/registrador_de_codons.h
@@ -0,0 +1,50 @@
+#include "definicoes.h"
+
+/**
+ * Pega a sequência dos genes
+ * e associa a sua respectiva
+ * proteína.
+ */
+int registre_os_codons(
+    /**
+     * Onde a sequência de amino ácidos será
+     * armazenada.
+     */
+    char registro_de_amin_acidos[][MAX_CODON],
+    /**
+     * Recebe a sequência de amino ácidos
+     */
+    char seq_amin_acid[],
+    /**
+     * Aqui recebe o número de amino ácidos lidos.
+     */
+    int qtd_amin_acidos,
+    /**
+     * Recebe o número de num_tentativas já ocorridas
+     * de decifrar o código genético da proteína.
+     */
+    int num_tentativas
+) {
+    /**
+     * Representa a posição do código genético
+     * que estamos a ler.
+     */
+    int
+        byte = num_tentativas,
+        con_while = 0;
+
+    for (int i = 0; i < qtd_amin_acidos; i++)
+    {
+        while (con_while != 3)
+        {
+            registro_de_amin_acidos[i][con_while] = seq_amin_acid[byte];
+
+            byte++;
+            con_while++;
+        }
+
+        con_while = 0;
+    }
+
+    return 0;
+}
